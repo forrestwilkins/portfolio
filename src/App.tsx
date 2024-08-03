1bbd2ea147c82c3d74c839bd396e39bc25a4e4a0
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-08-03 19:16:20 -0400

    feat: beep

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import './App.css';
+
 const App = () => {
-  return <>Hello world!</>;
+  const makeSound = () => {
+    var context = new AudioContext();
+    var oscillator = context.createOscillator();
+    oscillator.type = 'triangle';
+    oscillator.frequency.value = 800;
+    oscillator.connect(context.destination);
+    oscillator.start();
+    setTimeout(() => oscillator.stop(), 100);
+  };
+
+  return (
+    <div
+      style={{ display: 'flex', justifyContent: 'center', paddingTop: '200px' }}
+    >
+      <div
+        style={{
+          cursor: 'pointer',
+          userSelect: 'none',
+          fontSize: '40px',
+          padding: '10px 20px',
+          color: 'white',
+        }}
+        onClick={makeSound}
+      >
+        beep
+      </div>
+    </div>
+  );
 };
 
 export default App;
