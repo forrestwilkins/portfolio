e76cd54f1a5d7652115a1b8caf410076a5223d96
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-08-03 23:39:20 -0400

    refactor: simplify app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,54 @@
-import { useState } from 'react';
 import './App.css';
 
-const App = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  const makeSound = () => {
-    const context = new AudioContext();
-    const oscillator = context.createOscillator();
-    oscillator.type = 'sine';
-    oscillator.frequency.value = 700;
-    oscillator.connect(context.destination);
-    oscillator.start();
-
-    setTimeout(() => {
-      oscillator.frequency.value = 600;
-    }, 100);
-
-    setTimeout(() => {
-      oscillator.frequency.value = 500;
-    }, 200);
-
-    setTimeout(() => {
-      oscillator.frequency.value = 400;
-    }, 300);
-
-    setTimeout(() => {
-      oscillator.frequency.value = 300;
-    }, 400);
-
-    setTimeout(() => {
-      oscillator.stop();
-    }, 500);
-  };
-
-  const handleClick = () => {
-    setIsPlaying(!isPlaying);
-    makeSound();
-  };
-
-  return (
+const App = () => (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      paddingTop: '200px',
+    }}
+  >
     <div
       style={{
-        display: 'flex',
-        justifyContent: 'center',
-        paddingTop: '200px',
+        cursor: 'pointer',
+        userSelect: 'none',
+        fontSize: '40px',
+        padding: '10px 20px',
+        color: 'white',
+      }}
+      onClick={() => {
+        const context = new AudioContext();
+        const oscillator = context.createOscillator();
+
+        oscillator.type = 'sine';
+        oscillator.frequency.value = 500;
+        oscillator.connect(context.destination);
+        oscillator.start();
+
+        setTimeout(() => {
+          oscillator.frequency.value = 400;
+        }, 100);
+
+        setTimeout(() => {
+          oscillator.frequency.value = 300;
+        }, 200);
+
+        setTimeout(() => {
+          oscillator.frequency.value = 200;
+        }, 300);
+
+        setTimeout(() => {
+          oscillator.frequency.value = 50;
+        }, 400);
+
+        setTimeout(() => {
+          oscillator.stop();
+        }, 500);
       }}
     >
-      <div
-        style={{
-          cursor: 'pointer',
-          userSelect: 'none',
-          fontSize: '40px',
-          padding: '10px 20px',
-          color: 'white',
-        }}
-        onClick={handleClick}
-      >
-        beep
-      </div>
+      beep
     </div>
-  );
-};
+  </div>
+);
 
 export default App;
