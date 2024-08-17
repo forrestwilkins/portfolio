b84a5e805765e4f1675957c608b59f1a580e8666
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-08-16 23:58:47 -0400

    feat: redirect user to last visited page on page load

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { RouterProvider } from 'react-router-dom';
-import appRouter from './routes/app.router';
-import './globals.css';
-
-const App = () => <RouterProvider router={appRouter} />;
-
-export default App;
--- src/components/App/Layout.tsx
+++ src/components/App/Layout.tsx
@@ -1,8 +1,23 @@
 // TODO: Add styles for the layout
 
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 const Layout = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    localStorage.setItem('last-visited-page', location.pathname);
+  }, [location]);
+
+  useEffect(() => {
+    const lastVisitedPage = localStorage.getItem('last-visited-page');
+    if (lastVisitedPage) {
+      navigate(lastVisitedPage, { replace: true });
+    }
+  }, [navigate]);
+
   return (
     <div style={{ padding: '50px 50px 0' }}>
       <Outlet />
