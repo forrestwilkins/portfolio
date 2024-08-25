a2db53ad307801158eb4c614b33c8c9706146c2e
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-08-25 12:37:15 -0400

    feat: make dark theme default

--- src/components/App/theme-provider.tsx
+++ src/components/App/theme-provider.tsx
@@ -14,7 +14,7 @@ type ThemeProviderState = {
 };
 
 const initialState: ThemeProviderState = {
-  theme: 'system',
+  theme: 'dark',
   setTheme: () => null,
 };
 
@@ -22,7 +22,7 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 
 export const ThemeProvider = ({
   children,
-  defaultTheme = 'system',
+  defaultTheme = 'dark',
   storageKey = 'vite-ui-theme',
   ...props
 }: ThemeProviderProps) => {
