a342be28dc716f60a090ad8255d4126c25d824ac
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-08-31 22:20:33 -0400

    feat: draw grid

--- src/pages/canvas-page.tsx
+++ src/pages/canvas-page.tsx
@@ -7,22 +7,21 @@ const CanvasPage = () => {
       return;
     }
 
-    context.fillRect(
-      canvas.width * 0.25,
-      canvas.height * 0.25,
-      canvas.width * 0.5,
-      canvas.height * 0.5,
-    );
-    context.fillStyle = 'rgb(200, 200, 200)';
+    for (let y = 0; y < canvas.height; y += 8) {
+      for (let x = 0; x < canvas.width; x += 8) {
+        context.fillStyle = `rgb(${x}, ${y}, 100)`;
+        context.fillRect(x, y, 4, 4);
+      }
+    }
   };
 
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center pt-28">
       <Canvas
         width={500}
-        height={500}
-        className="border-2"
+        height={250}
         onMount={handleCanvasMount}
+        className="rounded-md"
       />
     </div>
   );
