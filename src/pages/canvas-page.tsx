cb7e0b6e9e811b443b31e804bc560b7a9bf2f52e
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-09-01 14:06:53 -0400

    feat: update curve on mouse move

--- src/pages/canvas-page.tsx
+++ src/pages/canvas-page.tsx
@@ -1,10 +1,8 @@
 import Canvas from '@/components/shared/canvas';
 import { getRandomRGB } from '@/utils/visual.utils';
-import { MouseEvent, useState } from 'react';
+import { MouseEvent } from 'react';
 
 const CanvasPage = () => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
   const handleCanvasMount = (canvas: HTMLCanvasElement) => {
     const context = canvas.getContext('2d');
     if (!context) {
@@ -29,21 +27,8 @@ const CanvasPage = () => {
     }
 
     const rect = canvas.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    setMousePosition({ x, y });
-  };
-
-  const handleFrameRender = (canvas: HTMLCanvasElement, frameCount: number) => {
-    if (frameCount % 7 !== 0) {
-      return;
-    }
-
-    const context = canvas.getContext('2d');
-    if (!context) {
-      return;
-    }
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
 
     for (let y = 0; y < canvas.height; y += 8) {
       for (let x = 0; x < canvas.width; x += 8) {
@@ -51,7 +36,6 @@ const CanvasPage = () => {
         context.beginPath();
         context.moveTo(x, y);
 
-        const { x: mouseX, y: mouseY } = mousePosition;
         context.quadraticCurveTo(250, 125, mouseX, mouseY);
         context.stroke();
       }
@@ -64,8 +48,7 @@ const CanvasPage = () => {
         width={500}
         height={250}
         onMount={handleCanvasMount}
-        onClick={handleCanvasClick}
-        onFrameRender={handleFrameRender}
+        onMouseMove={handleCanvasClick}
         className="rounded-md"
       />
     </div>
