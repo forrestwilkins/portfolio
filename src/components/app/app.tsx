22f5e8c9249a699311d29fb238650aef74dd4499
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-08-31 20:53:37 -0400

    feat: enable audio only for specific keys

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,7 +2,7 @@ import Layout from '@/components/app/layout';
 import ThemeProvider from '@/components/app/theme-provider';
 import useAppStore from '@/store/app.store';
 import { getToneJS } from '@/utils/shared.utils';
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 const App = () => {
@@ -12,26 +12,34 @@ const App = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const enableAudio = useCallback(async () => {
-    if (isAudioEnabled) {
-      return;
-    }
-
-    const Tone = await getToneJS();
-    await Tone.start();
-
-    setIsAudioEnabled(true);
-    console.log('Audio enabled.');
-  }, [isAudioEnabled, setIsAudioEnabled]);
-
   useEffect(() => {
-    window.addEventListener('keydown', enableAudio);
+    const enableAudio = async () => {
+      if (isAudioEnabled) {
+        return;
+      }
+
+      const Tone = await getToneJS();
+      await Tone.start();
+
+      setIsAudioEnabled(true);
+      console.log('Audio enabled.');
+    };
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (
+        ['Space', 'Enter', 'Key', 'Digit'].some((key) => e.code.includes(key))
+      ) {
+        enableAudio();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
     window.addEventListener('mousedown', enableAudio);
     return () => {
-      window.removeEventListener('keydown', enableAudio);
+      window.removeEventListener('keydown', handleKeyDown);
       window.removeEventListener('mousedown', enableAudio);
     };
-  }, [enableAudio]);
+  }, [isAudioEnabled, setIsAudioEnabled]);
 
   useEffect(() => {
     const lastVisitedPage = localStorage.getItem('last-visited-page');
