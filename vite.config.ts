e4d0d74aa3e4dfd8c1560d540170a477cd82e154
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-09-01 17:54:55 -0400

    chore: install pwa vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import dynamicImport from 'vite-plugin-dynamic-import';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import dynamicImport from 'vite-plugin-dynamic-import';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config
 export default defineConfig({
   plugins: [
     dynamicImport({
@@ -11,6 +12,30 @@ export default defineConfig({
         if (id.includes('./node_modules/tone')) {
           return true;
         }
+      },
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'rhizome',
+        short_name: 'rhizome',
+        description: 'An interactive art piece',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'src/assets/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'src/assets/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
       },
     }),
     react(),
