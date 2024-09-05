2319b1c6aaa7e443b2cdb8c868a89a2415a9dcdb
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-09-04 22:17:59 -0400

    feat: add color properties for ripples

--- src/pages/canvas-two.tsx
+++ src/pages/canvas-two.tsx
@@ -6,7 +6,9 @@ import { MouseEvent, useRef } from 'react';
 interface Ripple {
   x: number;
   y: number;
-  color: string;
+  red: number;
+  green: number;
+  blue: number;
   radius: number;
 }
 
@@ -21,9 +23,18 @@ const CanvasTwo = () => {
   const handleClick = (canvas: HTMLCanvasElement, e: MouseEvent<Element>) => {
     const x = e.clientX - canvas.offsetLeft;
     const y = e.clientY - canvas.offsetTop;
-    const color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    const red = Math.random() * 255;
+    const green = Math.random() * 255;
+    const blue = Math.random() * 255;
 
-    ripplesRef.current.push({ x, y, color, radius: 0 });
+    ripplesRef.current.push({
+      x,
+      y,
+      red,
+      green,
+      blue,
+      radius: 0,
+    });
   };
 
   const handleRender = (canvas: HTMLCanvasElement, frameCount: number) => {
@@ -36,12 +47,16 @@ const CanvasTwo = () => {
       const ripple = ripplesRef.current[i];
       ctx.beginPath();
       ctx.arc(ripple.x, ripple.y, ripple.radius, 0, 2 * Math.PI);
-      ctx.strokeStyle = ripple.color;
+      ctx.strokeStyle = `rgb(${ripple.red}, ${ripple.green}, ${ripple.blue})`;
       ctx.lineWidth = 2;
       ctx.stroke();
 
       if (frameCount % 4 === 0) {
         ripple.radius += 1;
+
+        ripple.red += Math.random() * 10 - 5;
+        ripple.green += Math.random() * 10 - 5;
+        ripple.blue += Math.random() * 10 - 5;
       }
     }
   };
