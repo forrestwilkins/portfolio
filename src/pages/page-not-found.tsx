1a864cc9b44a8255ab68e3537a4ad6c84ffac37d
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-09-21 16:28:47 -0400

    refactor: convert to mui for 4040 page

--- src/pages/page-not-found.tsx
+++ src/pages/page-not-found.tsx
@@ -1,11 +1,19 @@
+// TODO: Update to support dark mode
+
 import Layout from '@/components/app/layout';
+import { Typography } from '@mui/material';
 
 const PageNotFound = () => {
   return (
     <Layout>
-      <div className="pt-20 text-center font-mono text-5xl text-gray-50">
+      <Typography
+        paddingTop="80px"
+        textAlign="center"
+        fontFamily="monospace"
+        fontSize="48px"
+      >
         404
-      </div>
+      </Typography>
     </Layout>
   );
 };
