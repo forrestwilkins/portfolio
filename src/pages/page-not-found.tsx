a993070feb604c684e2d71834b90aaaedb63c3be
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-09-23 22:07:54 -0400

    refactor: remove todo comment

--- src/pages/page-not-found.tsx
+++ src/pages/page-not-found.tsx
@@ -1,21 +1,17 @@
-// TODO: Update to support dark mode
-
 import Layout from '@/components/app/layout';
 import { Typography } from '@mui/material';
 
-const PageNotFound = () => {
-  return (
-    <Layout>
-      <Typography
-        paddingTop="80px"
-        textAlign="center"
-        fontFamily="monospace"
-        fontSize="48px"
-      >
-        404
-      </Typography>
-    </Layout>
-  );
-};
+const PageNotFound = () => (
+  <Layout>
+    <Typography
+      paddingTop="80px"
+      textAlign="center"
+      fontFamily="monospace"
+      fontSize="48px"
+    >
+      404
+    </Typography>
+  </Layout>
+);
 
 export default PageNotFound;
