85d5467d3225b3af9be31f5d34991fccda7c093c
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-10-03 19:46:35 -0400

    chore: test removal of pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 import dynamicImport from 'vite-plugin-dynamic-import';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config
 export default defineConfig({
@@ -14,28 +13,31 @@ export default defineConfig({
         }
       },
     }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'rhizome',
-        short_name: 'rhizome',
-        description: 'An interactive art piece',
-        theme_color: '#000000',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   manifest: {
+    //     name: 'rhizome',
+    //     short_name: 'rhizome',
+    //     description: 'An interactive art piece',
+    //     theme_color: '#000000',
+    //     orientation: 'portrait',
+    //     icons: [
+    //       {
+    //         src: '/pwa-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: '/pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //       },
+    //     ],
+    //   },
+    //   devOptions: {
+    //     enabled: true,
+    //   },
+    // }),
     react(),
   ],
 
