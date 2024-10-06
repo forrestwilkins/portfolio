56f29dc4d0f01c22759a68aad1ca716bb7bad1ed
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-10-06 16:40:30 -0400

    feat: add nav menu for multiple actions and links

--- src/components/nav/top-nav.tsx
+++ src/components/nav/top-nav.tsx
@@ -1,51 +1,85 @@
-import { ModeToggle } from '@/components/app/mode-toggle';
 import { canvasRef } from '@/components/shared/canvas/canvas-ref';
-import Link from '@/components/shared/link';
-import { Fullscreen } from '@mui/icons-material';
-import { Box, Button, IconButton, SxProps } from '@mui/material';
-import { useLocation } from 'react-router-dom';
+import DarkMode from '@/components/shared/icons/dark-mode';
+import { useIsDarkMode } from '@/hooks/shared.hooks';
+import { sleep } from '@/utils/shared.utils';
+import {
+  Fullscreen,
+  HomeRounded,
+  LightModeOutlined,
+  Menu as MenuIcon,
+} from '@mui/icons-material';
+import { Box, IconButton, Menu, MenuItem, useColorScheme } from '@mui/material';
+import { MouseEvent, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const TopNav = () => {
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+
+  const { setMode } = useColorScheme();
+  const isDarkMode = useIsDarkMode();
   const location = useLocation();
+  const navigate = useNavigate();
 
-  const isHomePage = location.pathname === '/';
-  const isRipples = location.pathname === '/ripples';
   const iOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
+  const isRipples = location.pathname === '/ripples';
 
-  const leftNavStyles: SxProps = {
-    position: 'fixed',
-    left: '12px',
-    top: '12px',
-    zIndex: 10,
+  const handleModeToggleClick = async () => {
+    setAnchorEl(null);
+    await sleep(100);
+    setMode(isDarkMode ? 'light' : 'dark');
   };
 
-  const rightNavStyles: SxProps = {
-    ...leftNavStyles,
-    left: 'unset',
-    right: '12px',
-    display: 'flex',
+  const renderModeToggle = () => {
+    if (isDarkMode) {
+      return (
+        <>
+          <LightModeOutlined fontSize="small" sx={{ marginRight: '1.25ch' }} />
+          Light mode
+        </>
+      );
+    }
+    return (
+      <>
+        <DarkMode
+          fontSize="small"
+          sx={{ marginRight: '1.25ch', transform: 'rotateY(180deg)' }}
+        />
+        Dark mode
+      </>
+    );
   };
 
   return (
-    <>
-      {!isHomePage && (
-        <Link to="/" sx={leftNavStyles}>
-          <Button variant="contained" disableTouchRipple>
-            Home
-          </Button>
-        </Link>
-      )}
+    <Box position="fixed" right="12px" top="12px" zIndex={10} display="flex">
+      <IconButton
+        onClick={(e: MouseEvent<HTMLButtonElement>) =>
+          setAnchorEl(e.currentTarget)
+        }
+      >
+        <MenuIcon />
+      </IconButton>
 
-      <Box sx={rightNavStyles}>
+      <Menu
+        anchorEl={anchorEl}
+        open={!!anchorEl}
+        onClose={() => setAnchorEl(null)}
+      >
+        <MenuItem onClick={() => navigate('/')}>
+          <HomeRounded fontSize="small" sx={{ marginRight: '1.25ch' }} /> Home
+        </MenuItem>
+
         {isRipples && !iOS && (
-          <IconButton onClick={() => canvasRef.current?.requestFullscreen()}>
-            <Fullscreen />
-          </IconButton>
+          <MenuItem onClick={() => canvasRef.current?.requestFullscreen()}>
+            <Fullscreen fontSize="small" sx={{ marginRight: '1.25ch' }} />
+            Fullscreen
+          </MenuItem>
         )}
 
-        <ModeToggle />
-      </Box>
-    </>
+        <MenuItem onClick={handleModeToggleClick}>
+          {renderModeToggle()}
+        </MenuItem>
+      </Menu>
+    </Box>
   );
 };
 
