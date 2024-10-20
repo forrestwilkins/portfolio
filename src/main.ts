a355e964af7a3e5b66b735142b6856253b9d997e
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-10-20 15:13:29 -0400

    chore: ensure server can build and run with dev still working

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 import express from 'express';
-import { join } from 'path';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 import helloRouter from './routes/hello.routes';
 
 dotenv.config();
@@ -10,6 +11,8 @@ const app = express();
 
 app.use(cors());
 app.use('/api', helloRouter);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(join(__dirname, '../dist')));
 
 app.listen(process.env.SERVER_PORT);
