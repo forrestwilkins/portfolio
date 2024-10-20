eb1b20fa17efc5690ba23a05f125325b86263ddf
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-10-19 23:16:46 -0400

    chore: just use __dirname in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 import express from 'express';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+import { join } from 'path';
 import helloRouter from './routes/hello.routes';
 
 dotenv.config();
 
 const app = express();
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(cors());
 app.use('/api', helloRouter);
-app.use(express.static(join(__dirname, '../view')));
+app.use(express.static(join(__dirname, '../dist')));
 
 app.listen(process.env.SERVER_PORT);
 console.log(`Server running at http://localhost:${process.env.SERVER_PORT}`);
