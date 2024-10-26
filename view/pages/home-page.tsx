fc2cae0309967407f79b4599193bb61d73156939
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-10-26 00:27:03 -0400

    feat: create websocket with useRef

--- view/pages/home-page.tsx
+++ view/pages/home-page.tsx
@@ -1,29 +1,28 @@
 import { Box, SxProps, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Link from '../components/shared/link';
 import { useAboveBreakpoint } from '../hooks/shared.hooks';
-
-const socket = new WebSocket(`ws://localhost:3100/ws`);
-
-socket.onopen = () => {
-  console.log('connected');
-
-  socket.send('Hello from client');
-};
-
-socket.onmessage = (event) => {
-  console.log(`Message from server: ${event.data}`);
-};
-
-socket.onerror = (error) => {
-  console.error(`WebSocket error`, error);
-};
 
 const HomePage = () => {
   const [time, setTime] = useState<string>();
 
   const isAboveMd = useAboveBreakpoint('md');
   const isAboveLg = useAboveBreakpoint('lg');
+
+  const ws = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    ws.current = new WebSocket(`ws://localhost:3100/ws`);
+    ws.current.onmessage = (event) => {
+      console.log('Client received: ', event.data);
+    };
+
+    return () => {
+      if (ws.current && ws.current.readyState === WebSocket.OPEN) {
+        ws.current.close();
+      }
+    };
+  }, []);
 
   useEffect(() => {
     const init = async () => {
@@ -71,12 +70,25 @@ const HomePage = () => {
           right={10}
           width="fit-content"
           height={10}
-          borderRadius={0.5}
+          sx={{
+            cursor: 'pointer',
+          }}
           onClick={() => {
-            socket.send('Hello from client! 🎉');
+            ws.current?.send('Hello from client! 🎉');
           }}
         >
-          <Typography fontSize="8px" color="text.secondary">
+          <Typography
+            fontSize="8px"
+            color="text.secondary"
+            borderRadius="2px"
+            paddingX={0.3}
+            sx={{
+              '&:hover': {
+                color: 'text.primary',
+              },
+              transition: 'all 0.3s ease-in-out',
+            }}
+          >
             {time}
           </Typography>
         </Box>
