c539ef5d4c96c135bc4e597203b7a4a059c2f1bf
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-10-25 21:09:20 -0400

    feat: make that handshake

--- src/main.ts
+++ src/main.ts
@@ -28,5 +28,5 @@ wss.on('connection', (ws) => {
   ws.on('error', console.error);
 });
 
-app.listen(process.env.SERVER_PORT);
+server.listen(process.env.SERVER_PORT);
 console.log(`Server running at http://localhost:${process.env.SERVER_PORT}`);
