da7a7e0490ded37492ff59e20a28021e23f5fb15
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-10-26 21:10:53 -0400

    feat: add uuid for each web socket connection

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,23 @@ import express from 'express';
 import { createServer } from 'http';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
+import { v4 as uuidv4 } from 'uuid';
 import { WebSocket, WebSocketServer } from 'ws';
 import appRouter from './routes/app.routes';
+
+export class WebSocketWithId extends WebSocket {
+  id!: string;
+}
+
+export class WebSocketServerWithIds extends WebSocketServer<
+  typeof WebSocketWithId
+> {}
 
 dotenv.config();
 
 const app = express();
 const server = createServer(app);
-const wss = new WebSocketServer({ path: '/ws', server });
+const webSocketServer = new WebSocketServerWithIds({ path: '/ws', server });
 
 app.use(cors());
 
@@ -19,15 +28,17 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(join(__dirname, './view')));
 app.use('/api', appRouter);
 
-wss.on('connection', (ws) => {
-  ws.on('message', (data) => {
-    for (const client of wss.clients) {
+webSocketServer.on('connection', (webSocket) => {
+  webSocket.id = uuidv4();
+
+  webSocket.on('message', (data) => {
+    for (const client of webSocketServer.clients) {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data.toString());
       }
     }
   });
-  ws.on('error', console.error);
+  webSocket.on('error', console.error);
 });
 
 server.listen(process.env.SERVER_PORT);
