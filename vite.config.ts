89f119afb4489b4c4248939f55aee9d55c6cdaea
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-11-02 23:43:28 -0400

    chore: remove PWA setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react';
 import * as dotenv from 'dotenv';
 import { defineConfig } from 'vite';
 import dynamicImport from 'vite-plugin-dynamic-import';
-import { VitePWA } from 'vite-plugin-pwa';
 
 dotenv.config();
 
@@ -31,29 +30,6 @@ export default defineConfig({
         if (id.includes('./node_modules/tone')) {
           return true;
         }
-      },
-    }),
-    VitePWA({
-      srcDir: 'view',
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'rhizome',
-        short_name: 'rhizome',
-        description: 'An interactive art piece',
-        theme_color: '#000000',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
       },
     }),
     react(),
