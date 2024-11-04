65cfe0941681994098af2fe8fffbfb50d974e445
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-11-03 23:08:33 -0500

    feat: add initial cache service

---
+++ src/cache/cache.service.ts
@@ -0,0 +1,28 @@
+import { createClient, RedisClientType } from 'redis';
+
+let redisClient: RedisClientType;
+
+(async () => {
+  redisClient = createClient();
+  await redisClient.connect();
+
+  redisClient.on('error', (error) => {
+    console.error('Redis error', error);
+  });
+})();
+
+class CacheService {
+  async set(key: string, value: any) {
+    await redisClient.set(key, JSON.stringify(value));
+  }
+
+  async get(key: string) {
+    return redisClient.get(key);
+  }
+
+  async del(key: string) {
+    await redisClient.del(key);
+  }
+}
+
+export default CacheService;
