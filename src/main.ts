319180ff061ff9750cf3e8554182de4e125efc64
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-11-05 01:22:12 -0500

    refactor: move redis connect call

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,21 @@ import { createServer } from 'http';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import appRouter from './app.routes';
-import { cacheService } from './cache/cache.service'; // Adjust the path accordingly
+import { cacheService } from './cache/cache.service';
 import { WebSocketServerWithIds } from './pub-sub/pub-sub.models';
 import { pubSubService } from './pub-sub/pub-sub.service';
 
 dotenv.config();
 
 (async () => {
-  // Connect to Redis
-  await cacheService.connect();
-
   const app = express();
   const server = createServer(app);
   const webSocketServer = new WebSocketServerWithIds({ path: '/ws', server });
 
   app.use(cors());
+
+  // Connect to Redis
+  await cacheService.connect();
 
   // Serve static files and API routes
   const __dirname = dirname(fileURLToPath(import.meta.url));
