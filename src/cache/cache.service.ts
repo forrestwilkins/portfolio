9ee774fa6eafa5346435e70683a15a1af87d8b67
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-11-07 20:42:49 -0500

    feat: pass url field when creating redis client

--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -1,10 +1,15 @@
+import * as dotenv from 'dotenv';
 import { createClient, RedisClientType } from 'redis';
+
+dotenv.config();
 
 class CacheService {
   public client: RedisClientType;
 
   constructor() {
-    this.client = createClient();
+    this.client = createClient({
+      url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    });
 
     this.client.on('error', (error) => {
       console.error('Redis error', error);
