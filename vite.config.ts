98f70878fe6fc5915fe383b1206f0ab620f5fa7d
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-11-10 22:26:49 -0500

    chore: ensure unique file names on each build

--- vite.config.ts
+++ vite.config.ts
@@ -25,9 +25,9 @@ export default defineConfig({
     outDir: '../dist/view',
     rollupOptions: {
       output: {
-        entryFileNames: `[name].${Date.now()}.js`,
-        chunkFileNames: `[name].${Date.now()}.js`,
-        assetFileNames: `[name].${Date.now()}.[ext]`,
+        entryFileNames: `[name].js?${Date.now()}`,
+        chunkFileNames: `[name].js?${Date.now()}`,
+        assetFileNames: `[name].[ext]?${Date.now()}`,
       },
     },
   },
